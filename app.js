72e8e4ebe371b836e3a597f6c573352af7f1511e
Ishakufuki <ishakufuki@gmail.com>
2025-04-26 10:24:30 +0100

    initail commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+const PORT = 3010
+
+
+// 'POST, GET, PATCH, PUT, DELETE'
+const uri = "mongodb+srv://user:5ZP8y2HUsWIOUAU9@cict.ziphshv.mongodb.net/?retryWrites=true&w=majority&appName=CICT";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      
+    }
+  }
+  run().catch(console.dir);
+
+const studList = [
+    { id: 1, name: "John doe", age: 24},
+    { id: 2, name: "Micheal sun", age: 23},
+    { id: 3, name: "Timothy Rain", age: 22},
+    { id: 4, name: "Mark sunday", age: 21},
+    { id: 5, name: "Malan Mafwil", age: 20},
+];
+
+app.get('/',(req, res)=>{
+    res.send("HELLO WORLD")
+});
+
+app.get("/student-list",async(req, res)=> {
+   
+    try {
+      const result = await client
+      .db("CICT")
+      .collection("student")
+      .find()
+      .toArray();
+      console.log(result)
+      res.json({data: studList})
+    } catch (error) {
+      
+    }
+})
+app.post("/student", async (req, res)=>{
+  try {
+    const name = req.body.name;
+    const age = req.body.age;
+
+    if(!name || !age){
+        res.status(403).json({ message: "All fields are required"});
+        return;
+    }
+
+    const result = await client.db("CICT").collection("students").insertOne({
+      name:name, 
+      age:age,
+    });
+    
+    console.log(result);
+
+    res.status(200).json({message: "student added successfully"})
+
+  } catch (error) {
+    console.log(error)
+  }
+    
+
+
+});
+
+app.listen(PORT,()=>console.log("app is running on port"+PORT))
